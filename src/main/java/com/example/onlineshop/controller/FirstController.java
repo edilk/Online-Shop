8feb13ad64dd8f49fabc628bdfbd4a0b79f9737e
edilk <ed2803k@gmail.com>
2020-05-22 17:05:26 +0600

    MySQL database added

--- src/main/java/com/example/onlineshop/controller/FirstController.java
+++ src/main/java/com/example/onlineshop/controller/FirstController.java
@@ -1,11 +1,13 @@
 package com.example.onlineshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Transactional
 public class FirstController {
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
